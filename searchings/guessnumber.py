57d65d1d90e242d9334709591fb52c924d28fbc0
Varshithatangeti <tangeti2005@gmail.com>
2025-02-19 19:53:10 +0530

    Initial commit

--- searchings/guessnumber.py
+++ searchings/guessnumber.py
@@ -13,16 +13,16 @@
 
 # finding the square root of a number using binary search
 
-def sqrt(n):
-    left=0
-    right=n
-    while left<=right:
-        mid=(left+right)//2
-        if mid*mid==n:
-            return mid
-        elif mid*mid<n:
-            left=mid+1
-        else:
-            right=mid-1
-    return right
-print(sqrt(4))
+# def sqrt(n):
+#     left=0
+#     right=n
+#     while left<=right:
+#         mid=(left+right)//2
+#         if mid*mid==n:
+#             return mid
+#         elif mid*mid<n:
+#             left=mid+1
+#         else:
+#             right=mid-1
+#     return right
+# print(sqrt(4))
