9f90d2534901a35a7fc1ec2c268834d608d8a244
Varshithatangeti <tangeti2005@gmail.com>
2025-06-09 18:20:23 +0530

    K difference between the pairs in a number

---
+++ searchings/kdiffPairs.py
@@ -0,0 +1,10 @@
+def kdiffPairs(nums, k):
+    result = set()
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if abs(nums[i] - nums[j]) == k:
+                pair = tuple(sorted([nums[i], nums[j]]))  # fixed
+                result.add(pair)  # fixed
+    return len(result)
+
+print(kdiffPairs([3, 1, 4, 1, 5], 2))  # fixed
